807877dc72a66810cea86db5d56c1913342953ea
unknown <thien.nguyen.cs2704@gmail.com>
2021-10-01 23:18:22 +0700

    modify view for posts

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -58,18 +58,18 @@
               </div>
               <div class="col-4">
                 <div class="row">
-                  <div class="col-10">
-                    <div class="row">
-                      <div class="col-6">
+                  <div class="col-8">
+                    <div>
+                      <div>
                         <strong>{{isset($post->user_name) && $post->user_name ? $post->user_name :""}}</strong>
                       </div>
-                      <div class="col-6">
-                        <span class="p-1 border-radius btn-info">{{ isset($post->created_at) && $post->created_at ?  date('d/m/Y', strtotime($post->created_at)) : "" }}</span>
+                      <div class="w-100">
+                        <span class="p-1 btn-info">{{ isset($post->created_at) && $post->created_at ?  date('d/m/Y', strtotime($post->created_at)) : "" }}</span>
                       </div>
                     </div>
                   </div>
-                  <div class="col-2">
-                    <div class="float-right">
+                  <div class="col-4">
+                    <div class="d-inline-flex">
                       @if (isset($post->status) && $post->status === 'published')
                         <button class="btn btn-success">
                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16">
@@ -90,6 +90,14 @@
                           </svg>
                         </button>
                       @endif
+                      <button class="btn btn-danger ml-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        </svg>
+                      </button>
+                    </div>
+                    <div>
                     </div>
                   </div>
                 </div>
