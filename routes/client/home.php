71c53db7fd5e522d42f93c169ac90df46519abac
Em Le <em.le@hdwebsoft.co>
2021-10-19 21:04:56 +0700

    Test git

--- routes/client/home.php
+++ routes/client/home.php
@@ -17,7 +17,7 @@ Route::group([
     Route::get('/posts-being-edited', 'PostController@edited');
     Route::get('/editable-post', 'PostController@editable');
     Route::get('/event-partial', 'PostController@getEventPartial');
-    Route::get('/get-voucher-for-user', 'PostController@getVoucherForUser');
+    Route::get('/get-voucher-for-user', 'PostController@getVoucherForUser');  
 
     Route::post('/post/save-post', 'PostController@save');
     Route::post('/post/delete-post', 'PostController@delete');
